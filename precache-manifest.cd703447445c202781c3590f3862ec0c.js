87e91daacdd91ac1415a86503feab0e2923fbcd5
Nitish Malik <nitmalik@publicisgroupe.net>
2020-06-28 16:14:06 +0530

    Updates

---
+++ precache-manifest.cd703447445c202781c3590f3862ec0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246dc632cf3099eaa888ec4053b15681",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "086ae240fbc4590b94c6",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4d61495491d48909b4a5",
+    "url": "/robofriends/static/css/main.c1859649.chunk.css"
+  },
+  {
+    "revision": "086ae240fbc4590b94c6",
+    "url": "/robofriends/static/js/2.ca64cad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.ca64cad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b768ee2a3013c56dfd",
+    "url": "/robofriends/static/js/3.cb86380e.chunk.js"
+  },
+  {
+    "revision": "4d61495491d48909b4a5",
+    "url": "/robofriends/static/js/main.a0f53c03.chunk.js"
+  },
+  {
+    "revision": "dbc03d6766175c5eb614",
+    "url": "/robofriends/static/js/runtime-main.8bf4ab2a.js"
+  }
+]);
