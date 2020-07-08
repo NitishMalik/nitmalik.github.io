e7cdcb30fdfc9ddfe0db2f085c69511ae93bb0d5
Nitish Malik <41605928+NitishMalik@users.noreply.github.com>
2020-07-08 12:44:28 +0530

    Pwa (#4)
    * optimization changes
    
    * adding favicon , adding responsiveness and pwa support
    
    * Implementing typescript,pwa functionality and testing
    
    * adding typescript
    
    Co-authored-by: Nitish Malik <nitmalik@publicisgroupe.net>

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -5,34 +5,42 @@ import SearchBox from "../SearchBox";
 import Scroll from "../Scroll";
 import ErrorBoundary from "../ErrorBoundary";
 import Header from "../Header";
+import { IRobo } from "redux/interfaces";
 
-export class MainPage extends Component {
+interface IMainPageProps {
+  getRobos: () => void;
+  robots: IRobo[];
+  searchField: string;
+  onSearchChanged: (event: React.SyntheticEvent) => void;
+  isPending: boolean;
+}
+export class MainPage extends Component<IMainPageProps, {}> {
   componentDidMount() {
     this.props.getRobos();
   }
 
   filterRobots = () => {
     const { robots, searchField } = this.props;
-    return robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    return robots!.filter((robot) => {
+      return robot.name!.toLowerCase().includes(searchField.toLowerCase());
     });
   };
 
   render() {
-    const { onSearchChange, isPending } = this.props;
+    const { onSearchChanged, isPending } = this.props;
 
     return (
       <div className="tc">
         <Header />
-        <SearchBox searchChange={onSearchChange} />
+        <SearchBox onSearchInputChange={onSearchChanged} />
         <Scroll>
           {isPending ? (
             <h1>Loading</h1>
           ) : (
-            <ErrorBoundary>
-              <CardList robos={this.filterRobots()} />
-            </ErrorBoundary>
-          )}
+              <ErrorBoundary>
+                <CardList robos={this.filterRobots()} />
+              </ErrorBoundary>
+            )}
         </Scroll>
       </div>
     );
